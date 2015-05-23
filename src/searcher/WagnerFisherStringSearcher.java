c03df53e48c2ad832231f06a001b9d3f187ec9f7
tiaraju <tiarajums@gmail.com>
2015-05-23 19:43:15 -0300

    adding new files

---
+++ src/searcher/WagnerFisherStringSearcher.java
@@ -0,0 +1,13 @@
+package searcher;
+
+import java.util.List;
+
+public class WagnerFisherStringSearcher implements Searcher{
+
+	@Override
+	public List<String> searchPattern(String pattern, String text) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
