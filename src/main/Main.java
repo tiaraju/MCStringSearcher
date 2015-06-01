f3079750c8e2ff7cb314d9373442ae0bdcc025c6
Administrator <tiarajums@gmail.com>
2015-06-01 17:51:36 -0300

    Iniciando implementação do main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,42 +1,73 @@
 package main;
 
+import java.util.List;
+
 import searcher.BruteForceStringSearcher;
+import searcher.KnuthMorrisPratt;
+import searcher.RabinKarp;
 import searcher.Searcher;
 
 public class Main {
 
-	Searcher searcher;
+	private static Searcher searcher;
 
-	private void runBruteForceExperiment() {
+	private static List<String> runBruteForceExperiment(String text,
+			String pattern) {
 		searcher = new BruteForceStringSearcher();
-		
-	}
-
-	private void runBoyerMooreExperiment() {
+		return searcher.searchPattern(pattern, text);
 
 	}
 
-	private void runWagnerFisherExperiment() {
+	private static List<String> runKnuthMorrisExperiment(String text,
+			String pattern) {
+		searcher = new KnuthMorrisPratt();
+		return searcher.searchPattern(pattern, text);
+	}
 
+	private static List<String> runRabinKarpExperiment(String text,
+			String pattern) {
+		searcher = new RabinKarp();
+		return searcher.searchPattern(pattern, text);
+
+	}
+
+	private static String readPatternFile(String patternFilePath) {
+		// TODO return a string containing the whole file's content
+		return null;
+	}
+
+	private static String readBaseFile(String baseFilePath) {
+		// TODO return a string containing the whole file's content
+		return null;
+	}
+
+	private static void init(int option, String text, String pattern) {
+		switch (option) {
+		case 1:
+			runBruteForceExperiment(text, pattern);
+			break;
+		case 2:
+			runKnuthMorrisExperiment(text, pattern);
+			break;
+		case 3:
+			runRabinKarpExperiment(text, pattern);
+			break;
+		default:
+			System.out.println("Invalid Option. Try Again later.");
+			break;
+		}
 	}
 
 	public static void main(String[] args) {
 		int approach = Integer.parseInt(args[1].trim());
-		String patternFilePath = args[2];
-		String baseFilePath = args[3];
-		
-		switch (approach) {
-		case 1:
-			
-			break;
-		case 2:
-			break;
-			
-		case 3:
-			break;
-		default:
-			break;
-		}
+
+		String patternFilePath = args[2].trim();
+		String baseFilePath = args[3].trim();
+
+		String text = readBaseFile(baseFilePath);
+		String pattern = readPatternFile(patternFilePath);
+
+		init(approach, text, pattern);
 	}
 
 }
