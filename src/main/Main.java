cf13c151b11aeeaaec520c8095a1c088122e93d3
tiaraju <tiarajums@gmail.com>
2015-06-03 21:10:08 -0300

    adding memory watcher thread and new features

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,57 +4,59 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 import searcher.BruteForceStringSearcher;
 import searcher.KnuthMorrisPratt;
 import searcher.RabinKarp;
 import searcher.Searcher;
+import util.MemoryWatcher;
 
 public class Main {
 
 	private static Searcher searcher;
+	private static boolean contains = false;
 
-	private static boolean runBruteForceExperiment(String text,
-			String pattern) {
+	private static boolean runBruteForceExperiment(String text, String pattern) {
 		searcher = new BruteForceStringSearcher();
 		return searcher.searchPattern(pattern, text);
 
 	}
 
-	private static boolean runKnuthMorrisExperiment(String text,
-			String pattern) {
+	private static boolean runKnuthMorrisExperiment(String text, String pattern) {
 		searcher = new KnuthMorrisPratt();
 		return searcher.searchPattern(pattern, text);
 	}
 
-	private static boolean runRabinKarpExperiment(String text,
-			String pattern) {
+	private static boolean runRabinKarpExperiment(String text, String pattern) {
 		searcher = new RabinKarp();
 		return searcher.searchPattern(pattern, text);
 
 	}
 
-	private static String readPatternFile(String patternFilePath) throws IOException {
-		 byte[] encoded = Files.readAllBytes(Paths.get(patternFilePath));
-		 return new String(encoded, Charset.defaultCharset());
+	private static String readPatternFile(String patternFilePath)
+			throws IOException {
+		byte[] encoded = Files.readAllBytes(Paths.get(patternFilePath));
+		return new String(encoded, Charset.defaultCharset());
 	}
 
 	private static String readBaseFile(String baseFilePath) throws IOException {
-		 byte[] encoded = Files.readAllBytes(Paths.get(baseFilePath));
-		 return new String(encoded, Charset.defaultCharset());
+		byte[] encoded = Files.readAllBytes(Paths.get(baseFilePath));
+		return new String(encoded, Charset.defaultCharset());
 	}
 
 	private static void init(int option, String text, String pattern) {
 		switch (option) {
 		case 1:
-			System.out.println(runBruteForceExperiment(text, pattern));
+			contains = runBruteForceExperiment(text, pattern);
 			break;
 		case 2:
-			System.out.println(runKnuthMorrisExperiment(text, pattern));
+			contains = runKnuthMorrisExperiment(text, pattern);
 			break;
 		case 3:
-			runRabinKarpExperiment(text, pattern);
+			contains = runRabinKarpExperiment(text, pattern);
 			break;
 		default:
 			System.out.println("Invalid Option. Try Again later.");
@@ -67,11 +69,36 @@ public class Main {
 		String patternFilePath = args[1].trim();
 		String baseFilePath = args[2].trim();
 		String text;
+		MemoryWatcher memory = new MemoryWatcher();
+		Thread thread = new Thread(memory);
 		try {
-			text = readBaseFile(baseFilePath).replaceAll(System.getProperty("line.separator"), "");
-			String pattern = readPatternFile(patternFilePath).replaceAll(System.getProperty("line.separator"), "");;
-			init(approach, text, pattern); 
+			text = readBaseFile(baseFilePath).replaceAll(
+					System.getProperty("line.separator"), "");
+			String pattern = readPatternFile(patternFilePath).replaceAll(
+					System.getProperty("line.separator"), "");
+			;
+			thread.start();
+			init(approach, text, pattern);
+
+			if (thread != null) {
+				memory.stopWatcher();
+				thread.join();
+			}
+
+			List<Long> values = memory.getMemoryConsumptionValues();
+			Collections.sort(values);
+			long memoryConsumption = values.get(values.size() / 2);
+			long timeOfExecution = searcher.getTimeOfExecution();
+			int numberOfOperations = searcher.getNumberOfOperations();
+
+			System.out.println("Texto buscado: " + pattern + " \nTexto_busca: "
+					+ text + " \nResultado: " + contains
+					+ " \nTempo execução: " + timeOfExecution
+					+ " \nConsumo de Memória: "+memoryConsumption+"\n Número de Operações: "+numberOfOperations);
+
 		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 
--- src/util/MemoryWatcher.java
+++ src/util/MemoryWatcher.java
@@ -13,13 +13,22 @@ public class MemoryWatcher implements Runnable{
 		this.memoryConsumption = new ArrayList<Long>();
 		while(running){
 			memoryConsumption.add(Runtime.getRuntime().totalMemory());
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
 		
 	}
 	
 	
-	private void stopWatcher(){
+	public void stopWatcher(){
 		this.running = false;
+	}																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																				
+	
+	public List<Long> getMemoryConsumptionValues(){
+		return this.memoryConsumption;
 	}
 	
 	
